230d6593588225416f3c77b6e0c2e46e0fb315cc
pinklettuce <101542479+pinklettuce@users.noreply.github.com>
2022-07-07 20:24:59 -1000

    Update passwdgen.py

--- passwdgen.py
+++ passwdgen.py
@@ -1,2 +1,2 @@
 #! /usr/bin/env python3
-import sys, random as r;a='';exec('a+=chr(r.randint(33,126));'*int(sys.argv[1]));print(a)
+a='';exec("a+=chr(__import__('random').randint(30,128));"*int(__import__('sys').argv[1]));print("[+] Password: %s"%a)
